6d398ff83237611b69315757e9d26e71886c1034
facundo ramirez <facundor468@gmail.com>
2023-05-27 18:33:16 -0300

    Second_Commit

--- Prueba.js
+++ Prueba.js
@@ -1,61 +1,69 @@
-function calcular_prestamo (monto, cuotas){
+class productos{
 
-parseFloat = monto;
-parseInt = cuotas;
-
-let prestamo = 0;
-
-if(monto > 0 && cuotas == 3){
-    prestamo = monto + (monto * 0.15);
-    return prestamo
-    
-}
-else if(monto > 0 && cuotas == 6){
-    prestamo = monto + (monto * 0.35);
-    return prestamo
-}
-else if(monto > 0 && cuotas == 12){
-    prestamo = monto + (monto * 0.70);
-    return prestamo
-}
-else if(monto > 0 && cuotas == 1){
-    return prestamo
-}
-}
-
-function socio(prestamo, estado_usuario){
-    if(estado_usuario == "fin"){
-        let descuento = prestamo * 0.20;
-    }
-    else{
-        return 0
-    }
-}
-
-alert("gracias por elegirnos");
-console.log("bienvenido");
-
-let monto = ""
-
-while(monto != "fin"){
-
-    monto = prompt("ingrese el monto que desea retirar o fin para fianlizar");
-
-    if(monto != "fin"){
-    let estado_usuario = prompt("es socio: si/no");
-    let cuotas = prompt("ingrese la cantidad de cuotas en la que quiere abonar:1,3,6 o 12");
-
-    let resultado_prestamo = calcular_prestamo(monto, cuotas);
-    let resultado_descuento = socio(resultado_prestamo, estado_usuario)
-
-    console.log("pediste un total de: ", monto)
-    console.log("cuotas: ", cuotas)
-    console.log("devolves: ", resultado_prestamo)
-
-    if(resultado_descuento != 0){
-        console.log("al ser socio recibes un descuento del 20%, tu monto total es de: ", resultado_descuento)
-    }    
+    constructor(nombre, precio, stock){
+        this.nombre=nombre;
+        this.precio=precio;
+        this.stock=stock;
     }
 
-    
+    get_datos(){
+        console.log("DATOS DEL PRODUCTO:");
+        console.log("stock disponible: ", this.stock);        
+        console.log("nombre del producto: ", this.nombre);
+        console.log("precio del producto: ", this.precio);
+        console.log("")
+    }
+
+    get_stock(){
+
+        if(this.stock <= 0){
+            return false
+        }
+        else{
+            return true
+        }
+    }
+
+    venta(cantidad){
+        if(this.stock >= cantidad){
+            console.log("perfecto, hay stock disponible");
+            this.stock = this.stock - cantidad
+        }
+        else{
+            console.log("no se pudo realizar la compra por falta de stock");
+        }
+    }
+}
+
+let lista_productos = []
+
+lista_productos.push(new productos("microondas", 80000, 7))
+lista_productos.push(new productos("televisor", 150000, 5))
+lista_productos.push(new productos("luces led", 15000, 4))
+lista_productos.push(new productos("parlante", 20000, 10))
+
+for(let producto of lista_productos){
+    producto.get_datos();
+}
+
+function buscar_producto(producto){
+
+    return producto.nombre == compra_usuario
+}
+
+let compra_usuario = prompt("ingrese el nombre del producto que quiere comprar")
+
+let resultado_busqueda = lista_productos.find (buscar_producto)
+
+if (resultado_busqueda != undefined) {
+
+    if (resultado_busqueda.get_stock) {
+        let cantidad = prompt("ingrese la cantidad deseada del producto")
+        resultado_busqueda.venta(cantidad)
+
+        resultado_busqueda.get_datos();
+    }
+}
+else {
+    console.log("no se encontro el producto: ", compra_usuario)
 }
